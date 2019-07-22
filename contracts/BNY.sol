df7b68534ae1d59e8ffdbd1251600f90506c7f1c
lunamonkey <lunamonkey@gmail.com>
2019-07-22 22:18:25 +0100

    Chris - Small tidy, draft version.

--- contracts/BNY.sol
+++ contracts/BNY.sol
@@ -8,18 +8,18 @@ contract BNYprice is usingOraclize {
     address XBNYaddress = address(0x3B3A99EBAA5d8D3fd3DFc4E1590fa2eB211adc3D);
     uint256 canoffshore ;
     uint256 priceInUsd = 2;
-    uint256 public priceUINT ;
-     string public converted;
+    uint256 public priceUINT;
+    string public converted;
     string public priceETHXBT;
-
 
     event LogNewOraclizeQuery(string description);
     event LogNewKrakenPriceTicker(string price);
-     constructor()
+    constructor ()
         public
     {
         oraclize_setProof(proofType_Android | proofStorage_IPFS);
-        update(); // Update price on contract creation...
+        update(); // Update price on contract creation. Free first call.
+        // TESTNET
     }
 
     function __callback(
@@ -30,12 +30,12 @@ contract BNYprice is usingOraclize {
         public
     {
         require(msg.sender == oraclize_cbAddress(),"Only Oraclize");
-        update(); // Recursively update the price stored in the contract...
+        // Can't do this. It will run out of gas instantly.
+        // update(); // Recursively update the price stored in the contract...
         priceETHXBT = _result;
         emit LogNewKrakenPriceTicker(priceETHXBT);
         stringFloatToUnsigned(priceETHXBT);
         priceUINT = safeParseInt(priceETHXBT, 10);
-        
     }
 
     function update()
@@ -48,50 +48,48 @@ contract BNYprice is usingOraclize {
             emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
             oraclize_query(60, "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
         }
-        
+
     }
-   
+
     function offshoreBNY(uint256 value) public {
-        
-        (bool success, bytes memory data) =   BNYaddress.call(abi.encodeWithSignature("GetbalanceOf(address)",msg.sender));
+
+        (bool success, bytes memory data) =   BNYaddress.call(abi.encodeWithSignature("getBalanceOf(address)",msg.sender));
         bytes32 preUserBalance;
         uint offset = 32;
         assembly{
-        preUserBalance := mload(add(data, offset))
+            preUserBalance := mload(add(data, offset))
         }
 
         uint256 userBalance = uint256(preUserBalance);
 
         require(userBalance >= value);
         if(userBalance >= value){
-        
-        BNYaddress.call(abi.encodeWithSignature("reduceBNY(address,uint256)",msg.sender,value));
-        XBNYaddress.call(abi.encodeWithSignature("increaseXBNY(address,uint256)",msg.sender,(value*priceUINT)/10000000000 ));
+            BNYaddress.call(abi.encodeWithSignature("BNY_AssetSolidification(address,uint256)",msg.sender,value));
+            XBNYaddress.call(abi.encodeWithSignature("XBNY_AssetDesolidification(address,uint256)",msg.sender,(value*priceUINT)/10000000000 ));
         }
     }
-    
+
     function offshoreXBNY(uint256 value) public {
-        
-        (bool success, bytes memory data) =   XBNYaddress.call(abi.encodeWithSignature("GetbalanceOf(address)",msg.sender));
+        (bool success, bytes memory data) =   XBNYaddress.call(abi.encodeWithSignature("getBalanceOf(address)",msg.sender));
         bytes32 preUserBalance;
         uint offset = 32;
         assembly {
-        preUserBalance := mload(add(data, offset))
+            preUserBalance := mload(add(data, offset))
         }
 
         uint256 userBalance = uint256(preUserBalance);
-        
+
         require(userBalance >= value);
         if(userBalance >= value){
-        XBNYaddress.call(abi.encodeWithSignature("reduceXBNY(address,uint256)",msg.sender,value));
-        BNYaddress.call(abi.encodeWithSignature("increaseBNY(address,uint256)",msg.sender,(value*10000000000)/priceUINT));
+            XBNYaddress.call(abi.encodeWithSignature("XBNY_AssetSolidification(address,uint256)",msg.sender,value));
+            BNYaddress.call(abi.encodeWithSignature("BNY_AssetDesolidification(address,uint256)",msg.sender,(value*10000000000)/priceUINT));
         }
     }
-    
+
     function stringFloatToUnsigned(string memory _s)public payable {
         uint stringLength = bytes(_s).length;
         bytes memory _new_s = new bytes(stringLength);
-        
+
         uint k = 0;
 
         for (uint i = 0; i < bytes(_s).length; i++) {
@@ -100,9 +98,6 @@ contract BNYprice is usingOraclize {
             _new_s[k] = bytes(_s)[i];
             k++;
         }
-
         converted = string(_new_s);
     }
-
-    
 }
